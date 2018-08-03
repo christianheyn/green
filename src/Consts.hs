d10d6289cb14004976644b6b6743d921a9f8d178
Christian Heyn <chris.heyn.dd@gmail.com>
2018-08-03 15:52:59 +0200

    .green changes

--- src/Consts.hs
+++ src/Consts.hs
@@ -3,5 +3,5 @@ module Consts where
 _WEEK_UNITS = ["w", "week", "weeks"]
 _DAY_UNITS = ["d", "day", "days"]
 _HOUR_UNITS = ["h", "hour", "hours"]
-_MINUTES_UNITS = ["m", "min", "minute", "minutes"]
+_MINUTES_UNITS = ["m", "minute", "minutes"]
 _TIME_UNITS = _WEEK_UNITS ++ _DAY_UNITS ++ _HOUR_UNITS ++_MINUTES_UNITS
